b2604904033cf87d3f651c49b7d72af139b6a71f
Maria Mireles <mireles335@gmail.com>
2021-09-20 21:09:24 -0500

    JWTs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,7 +3,11 @@ const { gql } = require("apollo-server-express");
 
 // create our typeDefs
 const typeDefs = gql`
-type User {
+  type Auth {
+    token: ID!
+    user: User
+  }
+  type User {
     _id: ID
     username: String
     email: String
@@ -11,8 +15,8 @@ type User {
     thoughts: [Thought]
     friends: [User]
   }
-  
-type Thought {
+
+  type Thought {
     _id: ID
     thoughtText: String
     createdAt: String
@@ -33,6 +37,11 @@ type Thought {
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
   }
 `;
 
